2af294bfcf45dde9dde1c028e8db9d6e337f6fa2
Marc Smith <smivers624@gmail.com>
2015-11-24 22:36:27 +0800

    Enemy movement commit, incorrect!

--- js/app.js
+++ js/app.js
@@ -21,6 +21,12 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
+  this.x += Math.floor((Math.random() * 500) + 1) * dt;
+
+  if (this.x > 540) {
+    this.x = Math.floor((Math.random() * -500) + -100);
+    this.x += Math.floor((Math.random() * 1000) + 1) * dt;
+  };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -48,17 +54,20 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function() {
-  /* Some sort of reference to allowedKeys */
+  // Some sort of reference to allowedKeys
+  // Are we supposed to use if/else loops here when keys are pressed?
+  // if 'left' is pressed, move Player(100,0); TODO
 };
 
 // Now instantiate your objects.
 // X position would be a negative value, to prevent pop-in of enemy object.
 // Y position closely matches the layout of the path.
-var enemyTop = new Enemy(5, 61);
-var enemyMid = new Enemy(5, 144);
-var enemyBot = new Enemy(5, 228);
+var enemyTop = new Enemy(-105, 61);
+var enemyMid = new Enemy(-105, 144);
+var enemyBot = new Enemy(-105, 228);
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
+// var allEnemies = [new Enemy(5, 61), new Enemy(5, 144), new Enemy(5, 228)];
 allEnemies.push(enemyTop);
 allEnemies.push(enemyMid);
 allEnemies.push(enemyBot);
